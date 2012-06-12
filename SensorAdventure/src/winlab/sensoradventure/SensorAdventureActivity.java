beb6b6834f110c7179f464d8e4c710d7033574d1
David Shen <daft@MBP>
2012-06-12 13:59:08 -0400

    add empty project

---
+++ SensorAdventure/src/winlab/sensoradventure/SensorAdventureActivity.java
@@ -0,0 +1,13 @@
+package winlab.sensoradventure;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SensorAdventureActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
