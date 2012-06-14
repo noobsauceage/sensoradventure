96d04c07ea2887f227a4bcada36980c701dd9e66
David Shen <daft@MBP>
2012-06-14 11:29:33 -0400

    began work on preferences screen, get to prefs using button in main activity

--- SensorAdventure/src/winlab/sensoradventure/SensorAdventureActivity.java
+++ SensorAdventure/src/winlab/sensoradventure/SensorAdventureActivity.java
@@ -1,7 +1,10 @@
 package winlab.sensoradventure;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SensorAdventureActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,15 @@ public class SensorAdventureActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	Intent Prefs = new Intent(SensorAdventureActivity.this, Preferences.class);
+            	startActivity(Prefs);
+            }
+        });
+
     }
+    
 }
