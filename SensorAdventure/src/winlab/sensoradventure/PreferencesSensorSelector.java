9ecb13234e23a66611c5d13485a7554a92d0a3b6
David Shen <daft@MBP>
2012-06-14 12:14:48 -0400

    preferences

---
+++ SensorAdventure/src/winlab/sensoradventure/PreferencesSensorSelector.java
@@ -0,0 +1,11 @@
+package winlab.sensoradventure;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PreferencesSensorSelector extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.sensorlist);
+	}
+}
