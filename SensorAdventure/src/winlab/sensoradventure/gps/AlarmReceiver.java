e31f1e0f4535e364fa726f3f549a5adc09b24f4b
Malathi Dharmalingam <dharmalingam.malathi@gmail.com>
2012-06-21 22:05:08 -0400

    Coomit 1.4 June 21

--- SensorAdventure/src/winlab/sensoradventure/gps/AlarmReceiver.java
+++ SensorAdventure/src/winlab/sensoradventure/gps/AlarmReceiver.java
@@ -1,7 +1,6 @@
-/**
- * 
- */
 package winlab.sensoradventure.gps;
+ 
+import winlab.sensoradventure.gps.*;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
