4e3bea0dbb4b7a7e11cada95fbf99ec2b10bbccc
XianyiGao <xianyigmsn@hotmail.com>
2012-06-26 19:24:37 -0400

    update

---
+++ SensorAdventure/src/winlab/SensorGUI/Child.java
@@ -0,0 +1,20 @@
+package winlab.SensorGUI;
+
+public class Child {
+	private String field;
+	private String unit;
+
+	public Child(String field, String unit) {
+		this.field = field;
+		this.unit = unit;
+
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+}
---
+++ SensorAdventure/src/winlab/SensorGUI/Parent.java
@@ -0,0 +1,37 @@
+package winlab.SensorGUI;
+import java.util.ArrayList;
+
+public class Parent {
+
+	private ArrayList<Child> children = new ArrayList<Child>();
+	private String name;
+	private boolean state;
+
+	public Parent(String name, ArrayList<Child> children, boolean state) {
+		this.children = children;
+		this.state = state;
+		this.name = name;
+
+	}
+	
+    public void setState(boolean state1){
+    	state=state1;
+    }
+    
+	public Child getChild(int childPosition) {
+		return children.get(childPosition);
+	}
+
+	public boolean getState() {
+		return state;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public ArrayList<Child> getChildren() {
+		return children;
+	}
+
+}
