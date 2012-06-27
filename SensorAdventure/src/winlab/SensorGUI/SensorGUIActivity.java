d9182c9fdf3bd770a665cc56d8e3edd73b544053
GradeighClark <gradeighdclark@gmail.com>
2012-06-27 14:51:40 -0400

    Added menu button

--- SensorAdventure/src/winlab/SensorGUI/SensorGUIActivity.java
+++ SensorAdventure/src/winlab/SensorGUI/SensorGUIActivity.java
@@ -4,6 +4,7 @@ package winlab.SensorGUI;
  * guimain.xml
  * group_row.xml
  * child_row.xml
+ * menu.xml
  * DO NOT set the ELV's height to "wrap_content" unless you specify
  * the height of the parent/group. The program will appear as if
  * it is not working!
@@ -12,8 +13,17 @@ package winlab.SensorGUI;
 
 import java.util.ArrayList;
 
+import sgui.gui.Child;
+import sgui.gui.Parent;
+import sgui.gui.R;
+import sgui.gui.SensorAdapter;
+
 import android.app.ExpandableListActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class SensorGUIActivity extends ExpandableListActivity {
 	private SensorAdapter sensorAdapter;
@@ -27,17 +37,15 @@ public class SensorGUIActivity extends ExpandableListActivity {
 	public void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
 		setContentView(R.layout.guimain);
-		String[] Sensors = {"Accelerometer",
-				"Magnetic", "Orientation", "Gyroscope",
-				"Light", "Pressure", "Temperature",
-				"Proximity", "Gravity", "L. Accelerometer",
-				"Rotation", "Humidity",
-				"A. Temperature", "Microphone" };
+		String[] Sensors = { "Accelerometer", "Magnetic", "Orientation",
+				"Gyroscope", "Light", "Pressure", "Temperature", "Proximity",
+				"Gravity", "L. Accelerometer", "Rotation", "Humidity",
+				"A. Temperature", "Microphone", "GPS" };
 
 		normalSensor.add(new Child("Sampling Rate", "Hz"));
 
-		for(int i = 0; i< Sensors.length;i++)
-		parents.add(new Parent(Sensors[i], normalSensor, false));
+		for (int i = 0; i < Sensors.length; i++)
+			parents.add(new Parent(Sensors[i], normalSensor, false));
 
 		sensorAdapter = new SensorAdapter(this, parents);
 		expanded = new boolean[parents.size()];
@@ -46,6 +54,23 @@ public class SensorGUIActivity extends ExpandableListActivity {
 			sensorAdapter.value[i] = null;
 			expanded[i] = false;
 		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.layout.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.settings:
+			Toast.makeText(this, "Pushed", Toast.LENGTH_LONG).show();
+			break;
+		}
+		return true;
 	}
 
 	public void onContentChanged() {
@@ -64,7 +89,7 @@ public class SensorGUIActivity extends ExpandableListActivity {
 	}
 
 	public void update() {
-		for (int i = 0; i < parents.size(); i++){
+		for (int i = 0; i < parents.size(); i++) {
 			parents.get(i).setState(sensorAdapter.checkbox[i].isChecked());
 			if (expanded[i]) {
 				for (int j = 0; j < parents.size(); j++)
