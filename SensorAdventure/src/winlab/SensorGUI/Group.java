638bb9db52b69cec63bd37c92d62fec081b65ac4
GradeighClark <gradeighdclark@gmail.com>
2012-06-27 19:26:16 -0400

    Parent removed & renamed to Group for consistency

--- SensorAdventure/src/winlab/SensorGUI/Group.java
+++ SensorAdventure/src/winlab/SensorGUI/Group.java
@@ -1,23 +1,24 @@
 package winlab.SensorGUI;
+
 import java.util.ArrayList;
 
-public class Parent {
+public class Group {
 
 	private ArrayList<Child> children = new ArrayList<Child>();
 	private String name;
 	private boolean state;
 
-	public Parent(String name, ArrayList<Child> children, boolean state) {
+	public Group(String name, ArrayList<Child> children, boolean state) {
 		this.children = children;
 		this.state = state;
 		this.name = name;
 
 	}
-	
-    public void setState(boolean state1){
-    	state=state1;
-    }
-    
+
+	public void setState(boolean state1) {
+		state = state1;
+	}
+
 	public Child getChild(int childPosition) {
 		return children.get(childPosition);
 	}
