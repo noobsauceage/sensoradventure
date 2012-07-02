01d189aa58843b180689c5418eab01369f449232
XianyiGao <xianyigmsn@hotmail.com>
2012-07-02 15:34:35 -0400

    Timer

--- SensorAdventure/src/winlab/SensorGUI/StartGUI.java
+++ SensorAdventure/src/winlab/SensorGUI/StartGUI.java
@@ -1,44 +1,56 @@
 package winlab.SensorGUI;
 
+
+
 import winlab.sensoradventure.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class StartGUI extends Activity implements OnClickListener{
-	TextView text1,text2,text3;
+	TextView text2,text3;
+	Chronometer mChronometer;
 	Button mark,stop;
+	String[] times=new String[10];
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	String str1="";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startgui);
-        text1=(TextView) findViewById(R.id.textView1);
+        mChronometer = (Chronometer) findViewById(R.id.chronometer);
         text2=(TextView) findViewById(R.id.textView5);
         text3=(TextView) findViewById(R.id.textView6);
         mark =(Button) findViewById(R.id.button1);
+        mark.setOnClickListener(this);
         stop =(Button) findViewById(R.id.button2); 
-        
-        str1="Running Time: ";
-        text1.setText(str1);
-  
-        
+        stop.setOnClickListener(this);
+
+        for (int i=0; i<10;i++)
+        	times[i]="";
+        mChronometer.setBase(SystemClock.elapsedRealtime());
+        mChronometer.start();
     }
-    
+ 
+
     public void onClick(View a) {
+    	String output="";
     	switch (a.getId()) {
     	case R.id.button1:
-    		
+    		for (int j=9; j>0;j--)
+    			times[j]=times[j-1];
+    		times[0]=mChronometer.getInstantTime();
+    		for (int j=0; j<10;j++)
+    			output=output+times[j]+"\n";
+    		text2.setText(output);
     		break;
     		
     	case R.id.button2:
-    		
+    		mChronometer.stop();
     		break;
     	}
     }
 }
-
