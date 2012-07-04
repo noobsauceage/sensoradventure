bd53c61720ce165eed13f2cd59ddb9f807d46812
XianyiGao <xianyigmsn@hotmail.com>
2012-07-03 23:34:34 -0400

    try to fix sglite

--- SensorAdventure/src/winlab/SensorGUI/StartGUI.java
+++ SensorAdventure/src/winlab/SensorGUI/StartGUI.java
@@ -102,9 +102,11 @@ public class StartGUI extends Activity implements OnClickListener {
 			flag=false;
 			if (state[0]) stopService(new Intent(this,RunningService.class)); 
 			if (state[1]) {
+				try{
 				data2.endTransaction();
 				data2.copy();
 	        	data2.close();
+				} catch(Exception e){}
 	        	stopService(new Intent(this, Sensors_SQLite_Service.class));
 			}
 			mChronometer.stop();
