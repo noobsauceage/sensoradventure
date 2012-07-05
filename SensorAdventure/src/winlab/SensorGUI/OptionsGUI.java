d79be8489065aacdfd68c0bc6740141212765d42
GradeighClark <gradeighdclark@gmail.com>
2012-07-05 19:36:08 -0400

    Small documentation

--- SensorAdventure/src/winlab/SensorGUI/OptionsGUI.java
+++ SensorAdventure/src/winlab/SensorGUI/OptionsGUI.java
@@ -1,5 +1,11 @@
 package winlab.SensorGUI;
 
+/* This is a simple CheckBox GUI for the data configuration options.
+ * It retains CheckBox memory via the static boolean array state[].
+ * 
+ * Written by G.D.C.
+ */
+
 import winlab.sensoradventure.R;
 import android.app.Activity;
 import android.os.Bundle;
@@ -8,8 +14,11 @@ import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class OptionsGUI extends Activity {
-	public CheckBox cb[] = null;
-	public static boolean state[] = null;
+	public CheckBox cb[] = null; // Array to hold the three CBs
+	public static boolean state[] = null; // Static boolean array to retain
+											// state
+											// will not be destroyed when
+											// activity is exited.
 
 	/** Called when the activity is first created. */
 	@Override
@@ -17,14 +26,18 @@ public class OptionsGUI extends Activity {
 		super.onCreate(bundle);
 		setContentView(R.layout.options_main);
 
+		// Initialize cb
 		cb = new CheckBox[3];
 
+		// Populate cb
 		cb[0] = (CheckBox) findViewById(R.id.checkBox1);
 		cb[1] = (CheckBox) findViewById(R.id.checkBox2);
 		cb[2] = (CheckBox) findViewById(R.id.checkBox3);
 
 		for (int i = 0; i < 3; i++) {
 			cb[i].setOnCheckedChangeListener(checked);
+			// Check to see if the state variable exists
+			// If so, place the previous states inside of it
 			if (state == null)
 				cb[i].setChecked(false);
 			else
@@ -33,12 +46,15 @@ public class OptionsGUI extends Activity {
 
 	}
 
+	// Listener for when a checkbox is clicked
 	OnCheckedChangeListener checked = new OnCheckedChangeListener() {
 
 		public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+			// Create state array if it does not exist
 			if (state == null) {
 				state = new boolean[3];
 			}
+			// Change the state variable to match what the user does
 			for (int i = 0; i < cb.length; i++) {
 				if (cb[i] == arg0) {
 					arg0.setChecked(arg1);
