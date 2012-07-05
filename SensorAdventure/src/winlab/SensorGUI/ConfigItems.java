8e2d69c01a9743c909b2340b6185aebb8c9c112f
Malathi Dharmalingam <dharmalingam.malathi@gmail.com>
2012-07-05 16:29:55 -0400

    commit added Confile

---
+++ SensorAdventure/src/winlab/SensorGUI/ConfigItems.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package winlab.SensorGUI;
+ 
+/**
+ * @author malathidharmalingam
+ * @version 1.0.0
+ * 
+ */
+public class ConfigItems  {
+
+	private String provider;
+	private String lograte;
+	private String micsampling;
+	private String micchannel;
+	private String micencode;
+	private String otherlograte;
+	
+	public String getProvider() {
+		return provider;
+	}
+	public void setProvider(String provider) {
+		this.provider = provider;
+	}
+	public String getLograte() {
+		return lograte;
+	}
+	public void setLograte(String lograte) {
+		this.lograte = lograte;
+	}
+	public String getMicsampling() {
+		return micsampling;
+	}
+	public void setMicsampling(String micsampling) {
+		this.micsampling = micsampling;
+	}
+	public String getMicchannel() {
+		return micchannel;
+	}
+	public void setMicchannel(String micchannel) {
+		this.micchannel = micchannel;
+	}
+	public String getMicencode() {
+		return micencode;
+	}
+	public void setMicencode(String micencode) {
+		this.micencode = micencode;
+	}
+	public String getOtherlograte() {
+		return otherlograte;
+	}
+	public void setOtherlograte(String otherlograte) {
+		this.otherlograte = otherlograte;
+	}
+  
+	 
+}
