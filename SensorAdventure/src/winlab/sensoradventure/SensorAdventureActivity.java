40b5eb6fb3630f0b3a84493c82a836a35e73b65f
GradeighClark <gradeighdclark@gmail.com>
2012-07-13 11:12:57 -0400

    Save button implementation

--- SensorAdventure/src/winlab/sensoradventure/SensorAdventureActivity.java
+++ SensorAdventure/src/winlab/sensoradventure/SensorAdventureActivity.java
@@ -7,6 +7,7 @@ package winlab.sensoradventure;
  * -- G.D.C.
  */
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -49,12 +50,12 @@ public class SensorAdventureActivity extends ExpandableListActivity {
 	public static String micsampling = "44.1";
 	public static String micchannel = "MONO";
 	public static String micencode = "16";
-	public static String accelerorate = "1";	
+	public static String accelerorate = "1";
 	public static String gyrorate = "1";
 	public static String magnetorate = "1";
 	public static String otherlograte = "1";
 	public static String Last_lograte = "1";
-	public static String Servers= "Servers";
+	public static String Servers = "Servers";
 	private SensorSetting ok;
 	private String fileName = "Save.txt";
 	private File path = Environment
@@ -73,16 +74,30 @@ public class SensorAdventureActivity extends ExpandableListActivity {
 		normalSensor.add(new Child("Update   Rate", "ms"));
 		micSensor.add(new Child("Sampling Rate", "Hz"));
 
-		initializeGroups();
+		try {
+			initializeGroups();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		sensorAdapter = new SensorAdapter(this, groups);
+
+		try {
+			initializeTexts();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		expanded = new boolean[groups.size()];
 		setListAdapter(sensorAdapter);
 		for (int i = 0; i < groups.size(); i++) {
+			if(!file.isFile())
 			SensorAdapter.value[i] = otherlograte;
 			expanded[i] = false;
 		}
-		Last_lograte=otherlograte;
+		Last_lograte = otherlograte;
 		start = (Button) findViewById(R.id.button1);
 		start.setOnClickListener(startClick);
 		config = (Button) findViewById(R.id.button2);
@@ -94,18 +109,19 @@ public class SensorAdventureActivity extends ExpandableListActivity {
 	@Override
 	protected void onResume() {
 		super.onResume();
-		//Toast.makeText(this, "I am called", Toast.LENGTH_LONG).show();
-		if (Last_lograte.equals(otherlograte))
-		{}else
-		{
-		for (int i = 0; i < groups.size(); i++) {
-			SensorAdapter.value[i] = otherlograte;
-			if ((sensorAdapter.edittext[i] != null)&&(expanded[i]))  sensorAdapter.edittext[i].setText(otherlograte);
- 
-		}
-		Last_lograte=otherlograte;
+		// Toast.makeText(this, "I am called", Toast.LENGTH_LONG).show();
+		if (Last_lograte.equals(otherlograte)) {
+		} else {
+			for (int i = 0; i < groups.size(); i++) {
+				SensorAdapter.value[i] = otherlograte;
+				if ((sensorAdapter.edittext[i] != null) && (expanded[i]))
+					sensorAdapter.edittext[i].setText(otherlograte);
+
+			}
+			Last_lograte = otherlograte;
 		}
 	}
+
 	private OnClickListener startClick = new OnClickListener() {
 		public void onClick(View v) {
 
@@ -129,7 +145,7 @@ public class SensorAdventureActivity extends ExpandableListActivity {
 			intent.putExtra("micsampling", micsampling);
 			intent.putExtra("micchannel", micchannel);
 			intent.putExtra("micencode", micencode);
-			intent.putExtra("accelerorate",  accelerorate);
+			intent.putExtra("accelerorate", accelerorate);
 			intent.putExtra("gyrorate", gyrorate);
 			intent.putExtra("magnetorate", magnetorate);
 			intent.putExtra("otherlograte", otherlograte);
@@ -167,13 +183,20 @@ public class SensorAdventureActivity extends ExpandableListActivity {
 	private OnClickListener saveClick = new OnClickListener() {
 		public void onClick(View v) {
 			try {
-				FileWriter output = new FileWriter(file);
+				BufferedWriter output = new BufferedWriter(new FileWriter(file));
 				for (int i = 0; i < groups.size(); i++) {
 					if (groups.get(i).getState() == false)
 						output.write("0 ");
 					else
 						output.write("1 ");
 				}
+				output.newLine();
+				for (int i = 0; i < groups.size(); i++) {
+					output.write(SensorAdapter.value[i]);
+					output.newLine();
+
+				}
+
 				output.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -221,7 +244,7 @@ public class SensorAdventureActivity extends ExpandableListActivity {
 			groups.get(i).setState(sensorAdapter.checkbox[i].isChecked());
 			if (expanded[i]) {
 				for (int j = 0; j < groups.size(); j++)
-					if ((sensorAdapter.edittext[j] != null)&&(expanded[j])) {
+					if ((sensorAdapter.edittext[j] != null) && (expanded[j])) {
 
 						if (sensorAdapter.edittext[j].getText().toString()
 								.length() == 0)
@@ -235,28 +258,21 @@ public class SensorAdventureActivity extends ExpandableListActivity {
 
 	}
 
-	public void initializeGroups() {
+	public void initializeGroups() throws FileNotFoundException {
 		Scanner scanner;
 		if (file.isFile()) {
-			try {
-				scanner = new Scanner(file).useDelimiter("\\s*");
-				for (int i = 0; i < Sensors.length; i++) {
+			scanner = new Scanner(file).useDelimiter("\\s*");
+			for (int i = 0; i < Sensors.length; i++) {
 
-					if (file.isFile()) {
-						int b = scanner.nextInt();
-						if (i != 13)
-							groups.add(new Group(Sensors[i], normalSensor,
-									(b != 0)));
-						else
-							groups.add(new Group(Sensors[i], micSensor,
-									(b != 0)));
-
-					}
+				if (file.isFile()) {
+					int b = scanner.nextInt();
+					if (i != 13)
+						groups.add(new Group(Sensors[i], normalSensor, (b != 0)));
+					else
+						groups.add(new Group(Sensors[i], micSensor, (b != 0)));
 				}
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
+
 		} else {
 			for (int j = 0; j < Sensors.length; j++) {
 				if (j != 13)
@@ -265,6 +281,18 @@ public class SensorAdventureActivity extends ExpandableListActivity {
 					groups.add(new Group(Sensors[j], micSensor, false));
 			}
 		}
+
 	}
 
+	public void initializeTexts() throws FileNotFoundException {
+		Scanner scanner;
+		scanner = new Scanner(file);
+		scanner.nextLine();
+		if (file.isFile()) {
+			for (int i = 0; i < Sensors.length; i++) {
+				SensorAdapter.value[i] = scanner.nextLine();
+
+			}
+		}
+	}
 }
